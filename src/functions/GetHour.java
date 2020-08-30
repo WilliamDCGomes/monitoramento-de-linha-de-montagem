6a0923582053622ba03eea341098f7227b31ff34
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-08-30 18:54:21 -0300

    Corrigido a classe de pegar o horario

--- src/functions/GetHour.java
+++ src/functions/GetHour.java
@@ -7,6 +7,15 @@ public class GetHour {
         Calendar calendar = new GregorianCalendar();
         Date trialTime = new Date();
         calendar.setTime(trialTime);
+        if(calendar.get(Calendar.MINUTE)<10&&calendar.get(Calendar.HOUR_OF_DAY)<10){
+            return "0" + calendar.get(Calendar.HOUR_OF_DAY) + ":0" + calendar.get(Calendar.MINUTE);
+        }
+        if(calendar.get(Calendar.MINUTE)<10){
+            return calendar.get(Calendar.HOUR_OF_DAY) + ":0" + calendar.get(Calendar.MINUTE);
+        }
+        if(calendar.get(Calendar.HOUR_OF_DAY)<10){
+            return "0" + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
+        }
         return calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
     }
 }
