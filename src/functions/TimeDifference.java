d403dab808df5a3f7d1c935b7697fe372b92e1ff
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2020-08-31 21:11:54 -0300

    Implementado funcao de diferenca de horario

--- src/functions/TimeDifference.java
+++ src/functions/TimeDifference.java
@@ -1,6 +1,16 @@
 package functions;
 public class TimeDifference {
-    public String getDifference(){
-        
+    public String getDifference(String begin, String end){
+        String[] vetB = begin.split(":");
+        String[] vetE = end.split(":");
+        int minutesB = (Integer.parseInt(vetB[0])*60) + Integer.parseInt(vetB[1]);
+        int minutesE = (Integer.parseInt(vetE[0])*60) + Integer.parseInt(vetE[1]);
+        int dif = minutesE - minutesB;
+        int hour = 0;
+        while(dif>60){
+            dif-=60;
+            hour++;
+        }
+        return hour + ":" + dif;
     }
 }
